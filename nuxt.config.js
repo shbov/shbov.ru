22ad778140152f0ae98263640b84f4d81095c553
Andrew Shubnikov <a.shbov@yandex.ru>
2021-01-17 22:14:04 +0300

    add sass support

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [],
+  css: ['normalize.css/normalize.css', '@assets/scss/index.scss'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
