268962814935bab0492606fd47c86dcd18b06879
Andrew Shubnikov <a.shbov@yandex.ru>
2021-01-23 16:50:08 +0300

    index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="container">1</div>
+  <div class="container">
+    <div class="row">
+      <div class="col-4">
+        <h1>Hi, I’m Andrew 👋</h1>
+        <p>
+          Pariatur enim consequat ea fugiat adipisicing velit adipisicing id
+          quis. Voluptate irure amet consequat sint ut. Et pariatur esse
+          voluptate excepteur commodo anim fugiat dolore consectetur consectetur
+          adipisicing duis reprehenderit minim. Ullamco reprehenderit elit
+          incididunt ipsum cillum officia. Non aliqua nostrud officia in nisi.
+          Aute ex elit ut deserunt anim voluptate velit consequat.
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
